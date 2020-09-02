f13649a6666a0b7a22978e704f850e2a01abd9d6
Mark Suckerberg <tommjd54321@outlook.com>
2020-09-01 20:57:34 -0500

    fixes TGUI errors

--- tgui/packages/tgui/interfaces/FaxMachine.js
+++ tgui/packages/tgui/interfaces/FaxMachine.js
@@ -44,56 +44,57 @@ const FaxContent = (props, context) => {
   const { act, data } = useBackend(context);
 
   return (
-  <Section title="Fax Menu"
-    buttons={
-      <Button
-        onClick={() => act('auth')}
-        icon={data.authenticated ? "sign-out-alt" : "sign-in-alt"}
-        content={data.authenticated ? "Log Out" : "Log In"}
-        color={data.authenticated ? "bad" : "good"} />
-    }>
-    <LabeledList>
-      <LabeledListItem label="Authentication">
-        <Box color="label">
-          {data.authenticated ? data.authenticated : "None"}
-        </Box>
-      </LabeledListItem>
-      <LabeledListItem label="Network">
-        <Box color="label">
-          {data.network ? data.network : "Disconnected"}
-        </Box>
-      </LabeledListItem>
-      <LabeledListItem label="Currently Sending">
+    <Section title="Fax Menu"
+      buttons={
         <Button
-          icon={"eject"}
-          onClick={() => act('paper')}
-          content={data.paper} />
-        <Button
-          icon={'edit'}
-          onClick={() => act('rename')}
-          content={"Rename"}
-          disabled={!data.paperinserted}
-        />
-      </LabeledListItem>
-      <LabeledListItem label="Sending to">
-        <Button
-          icon={'print'}
-          onClick={() => act('dept')}
-          content={data.destination}
-          disabled={!data.authenticated} />
-      </LabeledListItem>
-      <LabeledListItem label="Action">
-        <Button
-          icon={data.cooldown && data.respectcooldown
-            ? 'clock-o'
-            : "envelope-o"}
-          onClick={() => act('send')}
-          disabled={(data.cooldown && data.respectcooldown) || (!data.paperinserted)}
-          content={data.cooldown && data.respectcooldown
-            ? "Realigning"
-            : "Send"} />
-      </LabeledListItem>
-    </LabeledList>
-  </Section>
-  )
-}
+          onClick={() => act('auth')}
+          icon={data.authenticated ? "sign-out-alt" : "sign-in-alt"}
+          content={data.authenticated ? "Log Out" : "Log In"}
+          color={data.authenticated ? "bad" : "good"} />
+      }>
+      <LabeledList>
+        <LabeledListItem label="Authentication">
+          <Box color="label">
+            {data.authenticated ? data.authenticated : "None"}
+          </Box>
+        </LabeledListItem>
+        <LabeledListItem label="Network">
+          <Box color="label">
+            {data.network ? data.network : "Disconnected"}
+          </Box>
+        </LabeledListItem>
+        <LabeledListItem label="Currently Sending">
+          <Button
+            icon={"eject"}
+            onClick={() => act('paper')}
+            content={data.paper} />
+          <Button
+            icon={'edit'}
+            onClick={() => act('rename')}
+            content={"Rename"}
+            disabled={!data.paperinserted}
+          />
+        </LabeledListItem>
+        <LabeledListItem label="Sending to">
+          <Button
+            icon={'print'}
+            onClick={() => act('dept')}
+            content={data.destination}
+            disabled={!data.authenticated} />
+        </LabeledListItem>
+        <LabeledListItem label="Action">
+          <Button
+            icon={data.cooldown && data.respectcooldown
+              ? 'clock-o'
+              : "envelope-o"}
+            onClick={() => act('send')}
+            disabled={(data.cooldown && data.respectcooldown)
+              || (!data.paperinserted)}
+            content={data.cooldown && data.respectcooldown
+              ? "Realigning"
+              : "Send"} />
+        </LabeledListItem>
+      </LabeledList>
+    </Section>
+  );
+};
