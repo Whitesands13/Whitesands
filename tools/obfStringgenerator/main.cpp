47defbc9077cd4d94dd2ad962aebd4fcc145a0be
Jack7D1 <59462654+Jack7D1@users.noreply.github.com>
2020-11-20 15:22:26 -0500

    adds deobfusticator tool

--- tools/obfStringgenerator/main.cpp
+++ tools/obfStringgenerator/main.cpp
@@ -3,34 +3,56 @@
 #include "md5.h"
 using namespace std;
 
-string encrypt_by_key(string, string);
-
-int main() {
-
-    string str = "";
-    string key = "";
-    string fallback = "";
-    cout << "Enter string to encrypt: ";
-    getline(cin, str);
-    cout << "Enter key to encrypt by (such as the world.URL of a certain BYOND server): ";
-    getline(cin, key);
-    cout << "And finally the fallback/target word (case sensitive!): ";
-    getline(cin, fallback);
-    cout << "Generated entry for obf_string_list: " << endl;
-    cout << "list(\"\", \"" << encrypt_by_key(str, key) << "\", \"" << md5(md5(key) + str) << "\", \"" << fallback << "\")";
-    cin >> key;
-
-    return 0;
+string obfs_by_key(string str = "", string key = "")
+{
+	string result = "";
+	key = md5(key);
+	for (unsigned int i = 0; i < str.length(); i++) {
+		int keyPtr = (i % key.length());
+		result = result + static_cast<char>((str[i] + key[keyPtr]) - 96);
+	}
+	return result;
 }
 
-
-string encrypt_by_key(string str = "", string key = "")
+string deobfs_by_key(string str = "", string key = "")
 {
-    string result = "";
-    key = md5(key);
-    for (unsigned int i = 0; i < str.length(); i++) {
-        int keyPtr = (i % key.length());
-        result = result + static_cast<char>((str[i] + key[keyPtr])-96);
-    }
-    return result;
+	string result = "";
+	key = md5(key);
+	for (unsigned int i = 0; i < str.length(); i++) {
+		int keyPtr = (i % key.length());
+		result = result + static_cast<char>((str[i] - key[keyPtr]) + 96);
+	}
+	return result;
+}
+
+int main() {
+	short opt = 0;
+	cout << "1 for obfustication, 2 for deobfustication: ";
+	cin >> opt;
+	cin.ignore(numeric_limits<streamsize>::max(), '\n');
+	if (opt == 1) {
+		string str = "";
+		string key = "";
+		string fallback = "";
+		cout << "Enter string to obfusticate: ";
+		getline(cin, str);
+		cout << "Enter key to obfusticate by (such as the world.URL of a certain BYOND server): ";
+		getline(cin, key);
+		cout << "And finally the fallback/target word (case sensitive!): ";
+		getline(cin, fallback);
+		cout << "Generated entry for obf_string_list: " << endl;
+		cout << "list(\"\", \"" << obfs_by_key(str, key) << "\", \"" << md5(md5(key) + str) << "\", \"" << fallback << "\")";
+	}
+	else if (opt == 2) {
+		string str = "";
+		string key = "";
+		cout << "Enter string to deobfusticate: ";
+		getline(cin, str);
+		cout << "Enter key to deobfusticate by: ";
+		getline(cin, key);
+		cout << "Deobfusticated string: " << endl;
+		cout << deobfs_by_key(str, key);
+	}
+	cin >> opt;
+	return 0;
 }
