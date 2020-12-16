MarkSuckerberg/WaspStation-1.0

got it nearly completely working

code/_onclick/hud/generic_dextrous.dm
@@ -5,14 +5,14 @@
 
 	using = new /obj/screen/drop()
 	using.icon = ui_style
-	using.screen_loc = ui_layout["ui_drone_drop"]
+	using.screen_loc_name = "ui_drone_drop"
 	using.hud = src
 	static_inventory += using
 
 	pull_icon = new /obj/screen/pull()
 	pull_icon.icon = ui_style
 	pull_icon.update_icon()
-	pull_icon.screen_loc = ui_layout["ui_drone_pull"]
+	pull_icon.screen_loc_name = "ui_drone_pull"
 	pull_icon.hud = src
 	static_inventory += pull_icon
 

Whitesands13/Whitesands

code/_onclick/hud/generic_dextrous.dm
@@ -5,14 +5,14 @@
 
 	using = new /obj/screen/drop()
 	using.icon = ui_style
-	using.screen_loc = ui_layout["ui_drop_throw"]
+	using.screen_loc_name = "ui_drop_throw"
 	using.hud = src
 	static_inventory += using
 
 	pull_icon = new /obj/screen/pull()
 	pull_icon.icon = ui_style
 	pull_icon.update_icon()
-	pull_icon.screen_loc = ui_layout["ui_drone_pull"]
+	pull_icon.screen_loc_name = "ui_drone_pull"
 	pull_icon.hud = src
 	static_inventory += pull_icon
 