15db3276882acd93755d7a42989279f55d7f0bd9
Tad Hardesty <tad@platymuus.com>
2020-12-22 13:22:15 -0800

    Use portable Python for map merge hooks, other tools (#55658)
    * Add tools/bootstrap/python
    
    * Add dependencies.sh entry for Python
    
    * Use bootstrapped Python for HitboxExpander
    
    * Use bootstrapped Python when installing hooks
    
    Also:
    - Adds tools/hooks/Uninstall.bat
    - Removes the need to re-run Install.bat when a .hook file changes
    
    Python path temporarily includes tools/mapmerge2 as before:
    - PYTHONPATH in tools/hooks/python.sh handles Linux/virtualenv
    - python36._pth handles Windows portable
    
    * Move DMI merge driver to its own directory
    
    * Use bootstrapped Python for makeChangelog.bat
    
    * Set window title in bootstrap PS scripts
    
    * Use slashes in hook paths
    
    * Use relative imports in mapmerge2
    
    * Use bootstrapped Python for mapmerge
    
    * Update mapmerge2 readme
    
    * Skip non-3way conflicts
    
    * Fix relative path issue in posthoc merger
    
    * Add missing call and %* to .bat files
    
    * When installing hooks, don't uninstall them all first
    
    * Make dependencies.sh POSIX
    
    * Move update_paths.py to its own folder
    
    * Use line buffering for Python stdout/stderr
    
    It was switching to fully-buffered mode due to being piped, meaning we
    wouldn't see output until the script ended. This is undesirable.
    
    * Use new path to dmi.test in CI suite
    
    * Validate system Python version in bootstrap script
    
    * Add install advice to bootstrap script
    
    * Update path to requirements.txt in CI
    
    * Add upgrader for existing installations

--- dependencies.sh
+++ dependencies.sh
@@ -1,13 +1,13 @@
-#!/bin/bash
+#!/bin/sh
 
 #Project dependencies file
 #Final authority on what's required to fully build the project
 
 # byond version
 # Extracted from the Dockerfile. Change by editing Dockerfile's FROM command.
-LIST=($(sed -n 's/.*byond:\([0-9]\+\)\.\([0-9]\+\).*/\1 \2/p' Dockerfile))
-export BYOND_MAJOR=${LIST[0]}
-export BYOND_MINOR=${LIST[1]}
+LIST="$(sed -n 's/.*byond:\([0-9]\+\)\.\([0-9]\+\).*/\1 \2/p' Dockerfile)"
+export BYOND_MAJOR=${LIST% *}
+export BYOND_MINOR=${LIST#* }
 unset LIST
 
 #rust_g git tag
@@ -19,5 +19,8 @@ export NODE_VERSION=12
 # PHP version
 export PHP_VERSION=7.2
 
-# SpacemanDMM git tag
-export SPACEMAN_DMM_VERSION=suite-1.4
+# Extools git tag
+export EXTOOLS_VERSION=v0.0.6
+
+# Python version for mapmerge and other tools
+export PYTHON_VERSION=3.6.8
