9601d3c737ea4c31c24775a1c7c35760fd93c7f7
Shayne Fitzgerald <robotech417@yahoo.com>
2021-02-14 16:14:40 -0500

    fuggin what

--- tools/ci/check_grep.sh
+++ tools/ci/check_grep.sh
@@ -76,6 +76,7 @@ do
 		# We've got a multi-z map, check each file in succession
 		for file in $(jq -r '.map_file[]' $json)
 		do
+			echo $file
 			if [ ! -f "_maps$(jq -r '.map_path' $json)/$file" ]
 			then
 				echo "found invalid file reference to $filename in _maps/$json"
